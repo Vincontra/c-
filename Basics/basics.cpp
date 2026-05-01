ea456abebbff93388c08c4226daced6e259d6872
vinay <vinaygadge2003@gmail.com>
2026-05-01 14:28:33 +0530

    basics

---
+++ Basics/basics.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+  
+  // this is the way to write a end of line  
+  // or just how to print a new line
+  // cout<<"Hello this is Vinay"<<endl;
+  // cout<<"idbfifdg";
+  // cout<<endl;
+  // cout<<"this is a test";
+  // return 0;
+
+  
+
+
+
+
+}
