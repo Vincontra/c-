156facdf160a7f2b4902476dbfd09fd3b5b00fb1
vinay <vinaygadge2003@gmail.com>
2026-05-01 18:42:19 +0530

    basics stuff done

--- Basics/basics.cpp
+++ Basics/basics.cpp
@@ -1,16 +1,103 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
 int main(){
   
   // this is the way to write a end of line  
+
   // or just how to print a new line
   // cout<<"Hello this is Vinay"<<endl;
   // cout<<"idbfifdg";
   // cout<<endl;
   // cout<<"this is a test";
+  // cout<<"Hello this is Vinay"<<endl<<"wow"<<endl<<"this is a test"<<endl;
   // return 0;
 
-  
+  //c++ is case sensative language
+  // means whatever you write in small letters or capital letters it will be treated as different
+
+  // using namespace std: // simple hai 
+  // compiler ko bolta hai ki jab bhi aapko cout, cin, endl,ka use karna hai to aapko std::cout, std::cin ka use karna padega
+  // but using namespace std; likhne ke baad aapko std::cout, std::cin,ka use nahi karna padega
+  // aap directly cout, cin, endl, string, vector, map, set use kr sakte hai   
+
+
+  // cout<<"hello world"<<"\n";
+  // cout<<"this is similar to endl but it is faster than endl"<<"\n";
+  // cout<<"Vinay"<<endl<<"Gadge";
+
+
+
+// cpp me varibles ko identifier bhi kehte hai
+
+
+
+// int age=44;
+// char grade='A';
+// cout <<age<<endl;
+// cout<<"my age is "<<age<<endl;
+// cout<<"my grade is "<<grade<<endl;
+
+// float PI=3.14f;
+// cout<<PI<<endl;
+// return 0;
+
+// bool 1 byte
+// bool isSafe=true; // true matlab 1 and false matlab 0
+// cout<<isSafe<<endl;
+
+//primtive data types in c++
+// int 4 bytes
+// char 1 byte      
+// float 4 bytes
+// double 8 bytes
+//bool 1 byte
+
+
+// type casting
+
+// double val=2222.9;
+// int newval=(int)val;
+// cout<<newval<<endl<<val<<endl;
+
+// input in c++
+// int age;
+// cout<<"Enter your age: "<<endl;
+// cin>>age;
+// cout<<"your age is "<<age;
+
+// string name; // string ka s small hota hai
+// cin>>name;
+// cout<<"your name is "<<name;
+
+
+//Operators in c++
+// +- wale pata hai
+// rel ational operators: >, <, >=, <=, ==, !=
+// logical operators: &&, ||, !
+
+// cout<<(2>4)<<endl;  // false matlab 0
+// cout<<(2<4)<<endl;  // true matlab 1
+
+// cout<<(2>1&&4>1)<<endl;
+// cout<<(2>1||4<1)<<endl;
+// cout<<!(2>1)<<endl; // false matlab 0
+
+// sum of two numbers
+
+// int a;
+// cin>>a;
+// int b;
+// cin>>b;
+// cout<<(a+b)<<endl;
+
+
+//unarey operators: ++, --, +, -
+
+// a++ --> pehle use baad me increment
+// ++a --> pehle increment baad me use
+
+
+
 
 
 
