f117527e94392e3264ba7b6993cee6d7d753d48c
vinay <vinaygadge2003@gmail.com>
2026-05-01 11:03:24 +0530

    first commit

---
+++ vinay.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+using namespace std;
+int main(){
+    cout<<"Hello, World!";
+    int count=10;
+    for (size_t i = 0; i < count; i++)
+    {
+      cout << "vinay this is a test" << endl;
+    }
+
+    return 0;
+
+}
+
