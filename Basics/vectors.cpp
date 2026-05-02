28f507b96fd462fc4bf08d854d95faac3b6c63db
vinay <vinaygadge2003@gmail.com>
2026-05-02 13:57:27 +0530

    vectors done

--- Basics/vectors.cpp
+++ Basics/vectors.cpp
@@ -2,6 +2,71 @@
 using namespace std;
 
 int main(){
-  
-  return 0;
+  // vector<int>v;
+  // v.push_back(10);
+  // vector<int>v2={1,2,3,4,5};
+  // cout<<v2.size()<<endl;
+  // vector<int>v3(3,INT_MAX);
+  // cout<<v3[1]<<endl;
+  // return 0;
+
+
+
+  // vector<char>v={'a','b','c'};
+  // // for(char c:v){
+  // //   cout<<c<<" "<<endl; 
+  // // }
+
+  // for(int i=0;i<v.size();i++){
+  //   cout<<v[i]<<endl;
+  // }
+
+  // Vector Functions
+  //vector<int>v={10,20,30,40,50};
+  // v.pop_back();    
+  // v.clear();
+
+  // v.push_back(60);
+  // v.push_back(70);
+
+  // cout<<v.front()<<endl;
+  // cout<<v.back();
+
+  // for(int i=0;i<v.size();i++){
+  //   cout<<v[i]<<endl;
+  // }
+
+
+  // vector<int>v;
+  // v.push_back(1);
+  // v.push_back(2);
+  // v.push_back(3);
+
+  // cout<<v.size()<<endl;
+  // cout<<v.capacity()<<endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
